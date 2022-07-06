8f956050b892f091df977be4a37d685ce160a78f
rioka <rioka@libero.it>
2022-07-06 13:21:57 +0100

    Add middleware to replace fake token with a valid one

--- PactNETPlayground.Producer.Tests/TestStartup.cs
+++ PactNETPlayground.Producer.Tests/TestStartup.cs
@@ -1,4 +1,5 @@
 ﻿using PactNETPlayground.Producer.Tests.Support;
+using PactNETPlayground.Shared;
 
 namespace PactNETPlayground.Producer.Tests;
 
@@ -15,13 +16,18 @@ internal class TestStartup {
     }
     
     public void ConfigureServices(IServiceCollection services) {
-        
+
+        // used to generate a valid token when a fake token is found in the request
+        services.AddScoped<ITokenProvider, TokenProvider>();
+        services.AddTransient<BearerTokenReplacementMiddleware>();
         _startup.ConfigureServices(services);
     }
     
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 
         app.UseMiddleware<ProviderStateMiddleware>();
+        app.UseMiddleware<BearerTokenReplacementMiddleware>();
+        
         _startup.Configure(app, env);
     }
 }

318313167c31ecf8db2f73c117814a4d3ab00c0d
rioka <rioka@libero.it>
2022-07-06 10:54:42 +0100

    Add new project for shared code

---
+++ PactNETPlayground.Shared/Security.cs
@@ -0,0 +1,40 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PactNETPlayground.Shared; 
+
+public class Security {
+
+    public static readonly SymmetricSecurityKey IssuerSigningKey = new(Encoding.UTF8.GetBytes("zoff-gentile-cabrini"));
+    public static readonly string Issuer = "https://fake.onelogin.org";
+   
+    public static string GetToken(string userId, string audience = "producer-api", IDictionary<string, string>? customClaims = null) {
+        
+        var claims = new List<Claim> {
+            new (ClaimTypes.Name, userId)
+        };
+
+        if (customClaims != null) {
+
+            claims.AddRange(customClaims.Select(kvp => new Claim(kvp.Key, kvp.Value)));
+        }
+        
+        var identity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Issuer = Issuer,
+            Audience = audience,
+            Subject = identity,
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(IssuerSigningKey, SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
